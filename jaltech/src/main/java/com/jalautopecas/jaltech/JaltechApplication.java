a4070ca25cdec86b4cd040e108a205052c93597a
Daniel <Daniel@DESKTOP-BDG18HP>
2019-03-15 09:12:26 -0300

    First commit

---
+++ jaltech/src/main/java/com/jalautopecas/jaltech/JaltechApplication.java
@@ -0,0 +1,13 @@
+package com.jalautopecas.jaltech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaltechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaltechApplication.class, args);
+	}
+
+}
